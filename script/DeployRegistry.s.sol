14667285dfa550f2541ba7e56083e1912bc7cb2c
Operator Filterer <operatorfilterer@gmail.com>
2022-11-04 16:44:00 -0700

    update deploy script

--- script/DeployRegistry.s.sol
+++ script/DeployRegistry.s.sol
@@ -4,11 +4,32 @@ pragma solidity ^0.8.17;
 import {OperatorFilterRegistry} from "../src/OperatorFilterRegistry.sol";
 import {ScriptBase} from "./ScriptBase.sol";
 
+interface ImmutableCreate2Factory {
+    function findCreate2Address(bytes32 salt, bytes memory initCode)
+        external
+        view
+        returns (address deploymentAddress);
+    function findCreate2AddressViaHash(bytes32 salt, bytes32 initCodeHash)
+        external
+        view
+        returns (address deploymentAddress);
+    function hasBeenDeployed(address deploymentAddress) external view returns (bool);
+    function safeCreate2(bytes32 salt, bytes memory initializationCode)
+        external
+        payable
+        returns (address deploymentAddress);
+}
+
 contract DeployRegistry is ScriptBase {
+    ImmutableCreate2Factory constant CREATE2_FACTORY =
+        ImmutableCreate2Factory(0x0000000000FFe8B47B3e2130213B802212439497);
+
     function run() public {
         setUp();
-        vm.startBroadcast(deployer);
-        OperatorFilterRegistry registry = new OperatorFilterRegistry();
-        vm.makePersistent(address(registry));
+        bytes memory creationCode = type(OperatorFilterRegistry).creationCode;
+        bytes32 salt = bytes32(0);
+
+        vm.broadcast(deployer);
+        CREATE2_FACTORY.safeCreate2(salt, creationCode);
     }
 }
