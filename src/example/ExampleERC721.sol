0509934238e391b8d4672e33ecf012972d74ce8b
Operator Filterer <operatorfilterer@gmail.com>
2022-11-04 13:41:24 -0700

    add example ERC721

---
+++ src/example/ExampleERC721.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import {ERC721} from "solmate/tokens/ERC721.sol";
+import {DefaultOperatorFilterer} from "../DefaultOperatorFilterer.sol";
+
+/**
+ * @title  ExampleERC721
+ * @notice This example contract is configured to use the DefaultOperatorFilterer, which automatically registers the
+ *         token and subscribes it to OpenSea's curated filters.
+ *         Adding the onlyAllowedOperator modifier to the transferFrom and both safeTransferFrom methods ensures that
+ *         the msg.sender (operator) is allowed by the OperatorFilterRegistry.
+ */
+contract ExampleERC721 is ERC721("Example", "EXAMPLE"), DefaultOperatorFilterer {
+    function transferFrom(address from, address to, uint256 tokenId) public override onlyAllowedOperator(msg.sender) {
+        super.transferFrom(from, to, tokenId);
+    }
+
+    function safeTransferFrom(address from, address to, uint256 tokenId)
+        public
+        override
+        onlyAllowedOperator(msg.sender)
+    {
+        super.safeTransferFrom(from, to, tokenId);
+    }
+
+    function safeTransferFrom(address from, address to, uint256 tokenId, bytes calldata data)
+        public
+        override
+        onlyAllowedOperator(msg.sender)
+    {
+        super.safeTransferFrom(from, to, tokenId, data);
+    }
+
+    function tokenURI(uint256) public pure override returns (string memory) {
+        return "";
+    }
+}
---
+++ test/example/ExampleERC721.t.sol
@@ -0,0 +1,49 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import {ExampleERC721} from "../../src/example/ExampleERC721.sol";
+import {DefaultOperatorFilterer} from "../../src/DefaultOperatorFilterer.sol";
+import {Ownable} from "openzeppelin-contracts/access/Ownable.sol";
+import {Test} from "forge-std/Test.sol";
+import {OperatorFilterRegistry, OperatorFilterRegistryErrorsAndEvents} from "../../src/OperatorFilterRegistry.sol";
+
+contract DefaultFilterer is DefaultOperatorFilterer, Ownable {
+    constructor() DefaultOperatorFilterer() {}
+
+    function filterTest(address addr) public onlyAllowedOperator(addr) returns (bool) {
+        return true;
+    }
+}
+
+contract DefaultOperatorFiltererTest is Test, OperatorFilterRegistryErrorsAndEvents {
+    ExampleERC721 example;
+    OperatorFilterRegistry registry;
+    address filteredAddress;
+
+    address constant DEFAULT_OPERATOR_FILTER_REGISTRY = address(0xdeadbeef);
+    address constant DEFAULT_SUBSCRIPTION = address(0xdadb0d);
+
+    function setUp() public {
+        vm.etch(DEFAULT_OPERATOR_FILTER_REGISTRY, address(new OperatorFilterRegistry()).code);
+        registry = OperatorFilterRegistry(DEFAULT_OPERATOR_FILTER_REGISTRY);
+
+        vm.startPrank(DEFAULT_SUBSCRIPTION);
+        registry.register(DEFAULT_SUBSCRIPTION);
+
+        filteredAddress = makeAddr("filtered address");
+        registry.updateOperator(address(DEFAULT_SUBSCRIPTION), filteredAddress, true);
+
+        example = new ExampleERC721();
+        vm.stopPrank();
+    }
+
+    function testFilter() public {
+        vm.startPrank(address(filteredAddress));
+        vm.expectRevert(abi.encodeWithSelector(AddressFiltered.selector, filteredAddress));
+        example.transferFrom(makeAddr("from"), makeAddr("to"), 1);
+        vm.expectRevert(abi.encodeWithSelector(AddressFiltered.selector, filteredAddress));
+        example.safeTransferFrom(makeAddr("from"), makeAddr("to"), 1);
+        vm.expectRevert(abi.encodeWithSelector(AddressFiltered.selector, filteredAddress));
+        example.safeTransferFrom(makeAddr("from"), makeAddr("to"), 1, "");
+    }
+}
