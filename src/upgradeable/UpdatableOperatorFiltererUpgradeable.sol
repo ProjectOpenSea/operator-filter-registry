b54a54228b9fb3bdff05ca046618f99a9f6a04cc
Alin Mihai Barbatei <alin.barbatei@sandbox.game>
2023-01-05 01:46:46 +0200

    added Updatable and Upgradable Operator Filterer

---
+++ src/upgradeable/UpdatableOperatorFiltererUpgradeable.sol
@@ -0,0 +1,90 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+import {IOperatorFilterRegistry} from "../IOperatorFilterRegistry.sol";
+import {Initializable} from "openzeppelin-contracts-upgradeable/proxy/utils/Initializable.sol";
+
+
+/**
+ * @title  UpdatableOperatorFiltererUpgradeable
+ * @notice Abstract contract whose init function automatically registers and optionally subscribes to or copies another
+ *         registrant's entries in the OperatorFilterRegistry. This contract allows the Owner to update the
+ *         OperatorFilterRegistry address via updateOperatorFilterRegistryAddress, including to the zero address,
+ *         which will bypass registry checks.
+ *         Note that OpenSea will still disable creator fee enforcement if filtered operators begin fulfilling orders
+ *         on-chain, eg, if the registry is revoked or bypassed.
+ * @dev    This smart contract is meant to be inherited by token contracts so they can use the following:
+ *         - `onlyAllowedOperator` modifier for `transferFrom` and `safeTransferFrom` methods.
+ *         - `onlyAllowedOperatorApproval` modifier for `approve` and `setApprovalForAll` methods.
+ *         Also use updateOperatorFilterRegistryAddress function to change registry address if needed 
+ */
+abstract contract UpdatableOperatorFiltererUpgradeable is Initializable {
+    error OperatorNotAllowed(address operator);
+    error OnlyOwner();
+
+    IOperatorFilterRegistry public operatorFilterRegistry;
+
+    function __UpdatableOperatorFiltererUpgradeable_init(address _registry, address subscriptionOrRegistrantToCopy, bool subscribe) 
+        internal
+        onlyInitializing
+    {
+        operatorFilterRegistry = IOperatorFilterRegistry(_registry);
+        // If an inheriting token contract is deployed to a network without the registry deployed, the modifier
+        // will not revert, but the contract will need to be registered with the registry once it is deployed in
+        // order for the modifier to filter addresses.
+        if (address(operatorFilterRegistry).code.length > 0) {
+            if (subscribe) {
+                operatorFilterRegistry.registerAndSubscribe(address(this), subscriptionOrRegistrantToCopy);
+            } else {
+                if (subscriptionOrRegistrantToCopy != address(0)) {
+                    operatorFilterRegistry.registerAndCopyEntries(address(this), subscriptionOrRegistrantToCopy);
+                } else {
+                    operatorFilterRegistry.register(address(this));
+                }
+            }
+        }
+    }
+
+    modifier onlyAllowedOperator(address from) virtual {
+        // Check registry code length to facilitate testing in environments without a deployed registry.
+        if (address(operatorFilterRegistry).code.length > 0) {
+            // Allow spending tokens from addresses with balance
+            // Note that this still allows listings and marketplaces with escrow to transfer tokens if transferred
+            // from an EOA.
+            if (from == msg.sender) {
+                _;
+                return;
+            }
+            if (!operatorFilterRegistry.isOperatorAllowed(address(this), msg.sender)) {
+                revert OperatorNotAllowed(msg.sender);
+            }
+        }
+        _;
+    }
+
+    modifier onlyAllowedOperatorApproval(address operator) virtual {
+        // Check registry code length to facilitate testing in environments without a deployed registry.
+        if (address(operatorFilterRegistry).code.length > 0) {
+            if (!operatorFilterRegistry.isOperatorAllowed(address(this), operator)) {
+                revert OperatorNotAllowed(operator);
+            }
+        }
+        _;
+    }
+
+    /**
+     * @notice Update the address that the contract will make OperatorFilter checks against. When set to the zero
+     *         address, checks will be bypassed. OnlyOwner.
+     */
+    function updateOperatorFilterRegistryAddress(address newRegistry) public virtual {
+        if (msg.sender != owner()) {
+            revert OnlyOwner();
+        }
+        operatorFilterRegistry = IOperatorFilterRegistry(newRegistry);
+    }
+
+    /**
+     * @dev assume the contract has an owner, but leave specific Ownable implementation up to inheriting contract
+     */
+    function owner() public view virtual returns (address);
+}
