90da83bb33cdd76da600771e406b4b6c37713e81
Ryan Ghods <ryan@ryanio.com>
2023-01-07 16:09:16 -0800

    Add upgradeable storage layout to RevokableOperatorFiltererUpgradeable

--- src/upgradeable/RevokableOperatorFiltererUpgradeable.sol
+++ src/upgradeable/RevokableOperatorFiltererUpgradeable.sol
@@ -4,6 +4,29 @@ pragma solidity ^0.8.13;
 import {OperatorFiltererUpgradeable} from "./OperatorFiltererUpgradeable.sol";
 
 /**
+ * @title  Upgradeable storage layout for RevokableOperatorFiltererUpgradeable.
+ * @notice Upgradeable contracts must use a storage layout that can be used across upgrades.
+ *         Only append new variables to the end of the layout.
+ */
+library RevokableOperatorFiltererUpgradeableStorage {
+    struct Layout {
+        /// @dev Whether the OperatorFilterRegistry has been revoked.
+        bool _isOperatorFilterRegistryRevoked;
+    }
+
+    /// @dev The storage slot for the layout.
+    bytes32 internal constant STORAGE_SLOT = keccak256("RevokableOperatorFiltererUpgradeable.contracts.storage");
+
+    /// @dev The layout of the storage.
+    function layout() internal pure returns (Layout storage l) {
+        bytes32 slot = STORAGE_SLOT;
+        assembly {
+            l.slot := slot
+        }
+    }
+}
+
+/**
  * @title  RevokableOperatorFilterer
  * @notice This contract is meant to allow contracts to permanently opt out of the OperatorFilterRegistry. The Registry
  *         itself has an "unregister" function, but if the contract is ownable, the owner can re-register at any point.
@@ -11,10 +34,10 @@ import {OperatorFiltererUpgradeable} from "./OperatorFiltererUpgradeable.sol";
  *         isOperatorFilterRegistryRevoked flag in order to permanently bypass the OperatorFilterRegistry checks.
  */
 abstract contract RevokableOperatorFiltererUpgradeable is OperatorFiltererUpgradeable {
+    using RevokableOperatorFiltererUpgradeableStorage for RevokableOperatorFiltererUpgradeableStorage.Layout;
+
     error OnlyOwner();
     error AlreadyRevoked();
-
-    bool private _isOperatorFilterRegistryRevoked;
 
     function __RevokableOperatorFilterer_init(address subscriptionOrRegistrantToCopy, bool subscribe) internal {
         OperatorFiltererUpgradeable.__OperatorFilterer_init(subscriptionOrRegistrantToCopy, subscribe);
@@ -22,7 +45,10 @@ abstract contract RevokableOperatorFiltererUpgradeable is OperatorFiltererUpgrad
 
     modifier onlyAllowedOperator(address from) override {
         // Check registry code length to facilitate testing in environments without a deployed registry.
-        if (!_isOperatorFilterRegistryRevoked && address(operatorFilterRegistry).code.length > 0) {
+        if (
+            !RevokableOperatorFiltererUpgradeableStorage.layout()._isOperatorFilterRegistryRevoked
+                && address(operatorFilterRegistry).code.length > 0
+        ) {
             // Allow spending tokens from addresses with balance
             // Note that this still allows listings and marketplaces with escrow to transfer tokens if transferred
             // from an EOA.
@@ -39,7 +65,10 @@ abstract contract RevokableOperatorFiltererUpgradeable is OperatorFiltererUpgrad
 
     modifier onlyAllowedOperatorApproval(address operator) override {
         // Check registry code length to facilitate testing in environments without a deployed registry.
-        if (!_isOperatorFilterRegistryRevoked && address(operatorFilterRegistry).code.length > 0) {
+        if (
+            !RevokableOperatorFiltererUpgradeableStorage.layout()._isOperatorFilterRegistryRevoked
+                && address(operatorFilterRegistry).code.length > 0
+        ) {
             if (!operatorFilterRegistry.isOperatorAllowed(address(this), operator)) {
                 revert OperatorNotAllowed(operator);
             }
@@ -54,14 +83,14 @@ abstract contract RevokableOperatorFiltererUpgradeable is OperatorFiltererUpgrad
         if (msg.sender != owner()) {
             revert OnlyOwner();
         }
-        if (_isOperatorFilterRegistryRevoked) {
+        if (RevokableOperatorFiltererUpgradeableStorage.layout()._isOperatorFilterRegistryRevoked) {
             revert AlreadyRevoked();
         }
-        _isOperatorFilterRegistryRevoked = true;
+        RevokableOperatorFiltererUpgradeableStorage.layout()._isOperatorFilterRegistryRevoked = true;
     }
 
     function isOperatorFilterRegistryRevoked() public view returns (bool) {
-        return _isOperatorFilterRegistryRevoked;
+        return RevokableOperatorFiltererUpgradeableStorage.layout()._isOperatorFilterRegistryRevoked;
     }
 
     /**
